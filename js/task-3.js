ae1e75f03080454c94b15297c3f2a10ac4438282
Serhii <kallisto77@gmail.com>
2025-08-22 13:08:13 +0400

    fix cicle with array

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
     const numbersNew = [];
-    for (let i = 0; i <= numbers.length; i++) { 
+    for (let i = 0; i < numbers.length; i++) { 
         if (numbers[i] > value) {
             numbersNew.push(numbers[i]);
         }
